e33a2d86e5457171eaba8b12acbc931da66eaf9f
Eduardo González <laloe.90@gmail.com>
2017-10-11 20:50:48 -0500

    bundle

--- ChihuahuaRiego/App_Start/BundleConfig.cs
+++ ChihuahuaRiego/App_Start/BundleConfig.cs
@@ -9,10 +9,17 @@ namespace ChihuahuaRiego
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.min.js",
+                        "~/Scripts/jquery.easing.min.js",
+                        "~/Scripts/jquery.bxslider.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/custom").Include(
+                        "~/Scripts/wow.js",
+                        "~/Scripts/custom.js"));
 
             // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
             // preparado para la producción y podrá utilizar la herramienta de compilación disponible en http://modernizr.com para seleccionar solo las pruebas que necesite.
@@ -21,7 +28,8 @@ namespace ChihuahuaRiego
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/bootstrap.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
